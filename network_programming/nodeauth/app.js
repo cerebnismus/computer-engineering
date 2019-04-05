cd0b0a897b2cf45170765ab7ecd915c807611733
Oguzhan Ince <oguzhanlarca@macintosh-local-34x.local>
2019-04-06 02:04:39 +0300

    startover @oguzhanlarca

--- network_programming/nodeauth/app.js
+++ network_programming/nodeauth/app.js
@@ -1,26 +1,99 @@
-var createError = require('http-errors');
+// 'use strict';
+// const http = require('http');
+// const url = require('url');
+// const path = require('path');
+// const fs = require('fs');
+// const detect = require('detect-port'); // https://www.npmjs.com/package/detect-port
 
+const mimeTypes = {
+  "html": "text/html",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpg",
+  "png": "image/png",
+  "js": "text/javascript",
+  "css": "text/css"
+};
+
+//Dependencies
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var passport = require('passport');
 var expressValidator = require('express-validator');
-var LocalStrategy = require('passport-local').Strategy;
 var multer = require('multer');
 var upload = multer({dest:'./uploads'});
 var flash = require('connect-flash');
+var bcrypt = require('bcryptjs');
 var mongodb = require('mongodb');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
+var ip = require('ip'); // https://github.com/indutny/node-ip
+var os = require('os'); // https://www.npmjs.com/package/os
+var ifaces = os.networkInterfaces(); // eth0 192.168.1.24
+var colors = require('colors'); // https://www.npmjs.com/package/colors
+var server = require('http').createServer(app); // https://www.w3schools.com/nodejs/met_http_createserver.asp
+var io = require('socket.io')(server); // https://socket.io/docs/internals/
+var port = process.env.PORT || 4444; // PORT=3000 node index.js ~> run with sudo for ports below 1024.
+// The process.env property returns an object containing the user environment.
+var session = require('express-session');  //"cookie-session" modülü tüm oturum bilgilerini istemcide saklar
+var logger = require('morgan');  // isteklerle ilgili logları konsola yazmak için
+var cookieParser = require('cookie-parser'); // https://www.npmjs.com/package/cookie-parser
+var bodyParser = require('body-parser');  //  get parameters from a request
+var nodemailer = require('nodemailer'); //  post parameters from a contact form
+var passport = require('passport') // http://www.passportjs.org/docs/configure/
+var LocalStrategy = require('passport-local').Strategy;
+var uptime = process.uptime(); // fail!!!
+
+colors.setTheme({
+  custom: ['red', 'underline'],
+  input: 'grey',
+  verbose: 'cyan',
+  info: 'green',  
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
+
+console.log('Sakarya University'.trap); // OMG Rainbows !
+console.log('Computer Engineering'.rainbow); // Drops the bass
+console.log('Network Programming'.info);
+console.log('oguzhan.ince@ogr.sakarya.edu.tr\n'.error);   
+
+server.listen(port, () => {
+  console.log(' ');
+  console.log('you can change that from server.js');
+  console.log('listening on *:' + port + ' \n');
+});
+
+console.log('--established inet status--'.warn);
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      // console.log(ifname + ':' + alias, iface.address);
+      console.log('### ' + 'http://localhost:' + port + '/');
+      console.log('### ' + 'http://' + iface.address + ':' + port + '/');
+    }else {
+      // this interface has only one ipv4 adress
+      // console.log(ifname, iface.address);
+      console.log('### ' + 'http://localhost:' + port + '/');
+      console.log('### ' + 'http://' + iface.address + ':' + port + '/');
+    }
+    ++alias;
+  });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -59,9 +132,9 @@ app.use(expressValidator({
 						 formParam += '[' + namespace.shift() + ']';
 						 }
 						 return {
-						 param : formParam,
-						 msg   : msg,
-						 value : value
+						   param : formParam,
+						   msg   : msg,
+						   value : value
 						 };
 						 }
 }));
@@ -71,6 +144,11 @@ app.use(require('connect-flash')());
 app.use(function (req, res, next) {
 		res.locals.messages = require('express-messages')(req, res);
 		next();
+});
+
+app.get('*', function(req, res, next){
+  res.locals.user = req.user || null;
+  next();
 });
 
 app.use('/', indexRouter);
@@ -90,6 +168,65 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Chatroom
+var numUsers = 0;
+
+// note, io(<port>) will create a http server for you
+io.on('connection', (socket) => {
+  var addedUser = false;
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    console.log(socket.username + ' is connected'.data + ' :) '.info);
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    console.log(socket.username + ' is typing'.data + '...'.debug);
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    console.log(socket.username + ' is stop typing'.data + '!'.debug);
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+      console.log(socket.username + ' is disconnected'.data + ' :( '.error);
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
 });
 
 module.exports = app;
